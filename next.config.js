083187468f0aa03e994a3052caee04ae156bf90c
nnhjs <nnhungjs@gmail.com>
2023-06-27 22:48:14 +0700

    feat: use /pages instead of /app

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  // trailingSlash: true,
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  reactStrictMode: true,
 }
- 
+
 module.exports = nextConfig
