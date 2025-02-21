67240ea929bf3c54655706418bd810de373f4fe5
nnhjs <nnhungjs@gmail.com>
2025-02-22 01:29:08 +0700

    blog

---
+++ blog/src/index.js
@@ -0,0 +1,15 @@
+// index.js
+
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname.startsWith("/api/")) {
+      return new Response(JSON.stringify({ name: "Cloudflare" }), {
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    return env.ASSETS.fetch(request);
+  },
+};
